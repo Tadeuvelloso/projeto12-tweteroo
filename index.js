9d24d325687096b64f89bf35b33bfb9b72afaaa0
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-11-03 17:55:16 -0300

    Get tweets complete

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ import cors from "cors";
 const app = express();
 app.use(cors());
 
+let tweets = [
+	{
+		username: "bobesponja",
+			avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		  tweet: "eu amo o hub"
+	}
+];
 
+app.get("/tweets", (req, res) => {
+    res.send(tweets);
 
+});
 
-app.listen("5000", () =>{
+app.listen(5000, () =>{
     console.log("Server runing in port: 5000");
 });
