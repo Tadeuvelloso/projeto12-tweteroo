8bba11b41cb10576a2632fac5676eb5ba010d559
Tadeuvelloso <tadeuvellososilva@gmail.com>
2022-11-07 17:53:09 -0300

    Versao final get com foto

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 let tweets = [
 	{
 		username: "bobesponja",
-			avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-		  tweet: "eu amo o hub"
+		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		tweet: "eu amo o hub"
 	}
 ];
 
-app.get("/tweets", (req, res) => {
-    res.send(tweets);
+let dataUser = [];
 
+app.post("/sign-up", (req, res) => {
+
+	const { username, avatar } = req.body
+
+	const newUser = {
+		username,
+		avatar,
+	}
+
+	dataUser.push(newUser);
+	res.send("OK")
+})
+
+app.post("/tweets", (req, res) => {
+	const { username, tweet } = req.body
+
+	if (!username || !tweet) {
+		res.status(400).send({ message: "Algum campo vazio!" });
+		return
+	}
+
+	const newTweet =
+	{
+		username,
+		tweet,
+	}
+
+
+	tweets.push(newTweet);
+
+	res.status(201).send("OK!");
+})
+
+
+app.get("/tweets", (req, res) => {
+
+	const lastTweets = tweets.slice(-10)
+	const tweetsCompletes = [];
+
+
+	for (let i = 0; i < lastTweets.length; i++){
+		const item = lastTweets[i];
+		if(item.username !== undefined && item.tweet !== undefined){
+		const avatarUser = dataUser.find((o) => item.username === o.username);
+		
+		const completeObj = {
+			username: item.username,
+			avatar: avatarUser.avatar,
+			tweet: item.tweet,
+		}
+
+		tweetsCompletes.push(completeObj);
+		}
+	}
+
+	res.status(200).send(tweetsCompletes);
 });
 
-app.listen(5000, () =>{
-    console.log("Server runing in port: 5000");
+app.listen(5000, () => {
+	console.log("Server runing in port: 5000");
 });
